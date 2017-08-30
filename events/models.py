a36722c5c7d45c83b14b2ecd8390395df14550cb
Santiago Angel <santiagoangel10@gmail.com>
2017-08-30 04:38:06 +0200

    end tutorial

--- events/models.py
+++ events/models.py
@@ -6,3 +6,14 @@ class BasicEvent(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     date = models.DateTimeField('event date')
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
