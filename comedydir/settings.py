55bacbf46074653cad3e454e2eee9d330572b7e2
Santiago Angel <santiagoangel10@gmail.com>
2017-08-30 03:46:48 +0200

    DB is working

--- comedydir/settings.py
+++ comedydir/settings.py
@@ -75,10 +75,11 @@ WSGI_APPLICATION = 'comedydir.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': '',
         'NAME': 'comedy',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- events/models.py
+++ events/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class BasicEvent(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    date = models.DateTimeField('event date')
