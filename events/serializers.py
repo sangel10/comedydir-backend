e64c259429e2cb30aadd0832bfb320fbdec2ec47
Santiago Angel <santiagoangel10@gmail.com>
2017-10-24 18:33:04 +0200

    Output the distance difference. Seems to range from .8 - 1.6, so we’ll just double the radius to be safe when using PostGis queries

--- events/serializers.py
+++ events/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from events.models import FacebookEvent, FacebookPlace
+from django.contrib.gis.geos import GEOSGeometry, Point
+from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
+# from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
+# from geopy.distance import distance
+from django.contrib.gis.db.models.functions import Distance
 
 class FacebookPlaceSerializer(serializers.ModelSerializer):
-    distance_from_target = serializers.SerializerMethodField()
+    # distance_from_target = serializers.SerializerMethodField()
+    # distance = serializers.SerializerMethodField()
+    distance_from_t = serializers.SerializerMethodField()
     class Meta:
         model = FacebookPlace
         fields = (
@@ -15,19 +23,46 @@ class FacebookPlaceSerializer(serializers.ModelSerializer):
             'facebook_street',
             'facebook_id',
             'facebook_region',
-            'distance_from_target',
+            # 'distance_from_target',
+            # 'distance',
+            'distance_from_t',
             'pk',
         )
-    def get_distance_from_target(self, obj):
-        latitude = self.context['request'].query_params.get('latitude', None)
-        longitude = self.context['request'].query_params.get('longitude', None)
-        if longitude is None or latitude is None:
-            return None
-        return obj.distance_from_target(latitude, longitude)
+    def get_distance_from_t(self, obj):
+        return obj.distance_from_t or None
+    #
+    # def get_distance_from_target(self, obj):
+    #     latitude = self.context['request'].query_params.get('latitude', None)
+    #     longitude = self.context['request'].query_params.get('longitude', None)
+    #     if longitude is None or latitude is None:
+    #         return None
+    #     return obj.distance_from_target(latitude, longitude)
+
+    # def get_distance(self, obj):
+    #     latitude = self.context['request'].query_params.get('latitude', None)
+    #     longitude = self.context['request'].query_params.get('longitude', None)
+    #     if longitude is None or latitude is None:
+    #         return None
+    #     # pnt = GEOSGeometry('POINT({} {})'.format(latitude, longitude), srid=4326)
+    #     pnt = Point(float(latitude), float(longitude), srid=4326)
+    #     dist = pnt.distance(obj.point)
+    #     # return 'boo'
+    #     # dist = Distance(obj.point, pnt)
+    #     # obj_point = Point(obj.point.x, obj.point.y, srid=4326)
+    #
+    #     # print('point', obj_point, pnt)
+    #     # dist =  Distance(obj_point, pnt)
+    #     # print('DISTANCE', dist)
+    #     # import pdb; pdb.set_trace()
+    #     # dist = obj.point.distance(pnt).m
+    #     # dist = Distance(m=distance(obj.point, pnt).meters)
+    #     return dist
 
 class FacebookEventSerializer(serializers.ModelSerializer):
     facebook_place = FacebookPlaceSerializer(read_only=True)
     distance_from_target = serializers.SerializerMethodField()
+    distance_model = serializers.SerializerMethodField()
+    distance_difference = serializers.SerializerMethodField()
 
     class Meta:
         model = FacebookEvent
@@ -41,8 +76,22 @@ class FacebookEventSerializer(serializers.ModelSerializer):
             'image_url',
             'pk',
             'distance_from_target',
-
+            'distance_model',
+            'distance_difference',
         )
+
+
+    def get_distance_difference(self, obj):
+        if not obj.facebook_place:
+            return None
+        latitude = self.context['request'].query_params.get('latitude', None)
+        longitude = self.context['request'].query_params.get('longitude', None)
+        if longitude is None or latitude is None:
+            return None
+        distance_from_t = obj.facebook_place.distance_from_target(latitude, longitude)
+        distance = obj.distance.km
+        return distance/distance_from_t
+
     def get_distance_from_target(self, obj):
         if not obj.facebook_place:
             return None
@@ -51,3 +100,12 @@ class FacebookEventSerializer(serializers.ModelSerializer):
         if longitude is None or latitude is None:
             return None
         return obj.facebook_place.distance_from_target(latitude, longitude)
+
+    def get_distance_model(self, obj):
+        if not obj.facebook_place:
+            return None
+        latitude = self.context['request'].query_params.get('latitude', None)
+        longitude = self.context['request'].query_params.get('longitude', None)
+        if longitude is None or latitude is None:
+            return None
+        return obj.distance.km
