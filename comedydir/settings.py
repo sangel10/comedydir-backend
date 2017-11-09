6adacde2ab24a0c2f565d6022deadc36abefc002
Santiago Angel <santiagoangel10@gmail.com>
2017-11-09 01:27:17 +0100

    GDAL Heroku config

--- comedydir/settings.py
+++ comedydir/settings.py
@@ -161,10 +161,6 @@ MAP_WIDGETS = {
     "GOOGLE_MAP_API_KEY": GOOGLE_MAP_API_KEY
 }
 
-# Do not set on server so they default to NONE
-# https://www.alextomkins.com/2017/08/fixing-gdal-geos-django-macos/
-GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 
 INTERNAL_IPS = ['127.0.0.1']
 
@@ -198,5 +194,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 #     '127.0.0.1:9000'
 # )
 
+# Do not set on server so they default to NONE
+# https://www.alextomkins.com/2017/08/fixing-gdal-geos-django-macos/
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+# GEOS_LIBRARY_PATH = "{}/libgeos_  c.so".format(os.environ.get('GEOS_LIBRARY_PATH'))
+# GDAL_LIBRARY_PATH = "{}/libgdal.so".format(os.environ.get('GDAL_LIBRARY_PATH'))
